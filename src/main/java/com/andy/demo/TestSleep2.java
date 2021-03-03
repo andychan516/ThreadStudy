7cbf51b4a74a50eff0b2ce534bba6b02966d9bbb
andychan516 <623247195@qq.com>
2021-03-03 15:36:54 +0800

    +线程睡眠

---
+++ src/main/java/com/andy/demo/TestSleep2.java
@@ -0,0 +1,21 @@
+package com.andy.demo;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * 模拟倒计时
+ */
+public class TestSleep2 {
+  public static void main(String[] args) throws InterruptedException {
+    tenDown();
+  }
+
+  public static void tenDown() throws InterruptedException {
+
+    while (true){
+      Date nowTime=new Date(System.currentTimeMillis());
+
+    }
+  }
+}
